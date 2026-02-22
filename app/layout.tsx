e0dab81349f5b8c6f3a4395cc674b6d8fc1ac9c4
Ingrid <calzada.ingrid10@gmail.com>
2026-02-22 14:48:44 -0500

    added open graph and google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,29 @@ const bebas = Bebas_Neue({
   display: "swap",
 });
 
+export const metadata = {
+  title: "Conecta con nuevas personas a través del deporte",
+  description: "Pleimeit es la app deportiva que conecta personas a través del deporte. Crea tus propios eventos o únete a uno de ellos.",
+  openGraph: {
+    title: "Conecta con nuevas personas a través del deporte",
+    description: "Pleimeit es la app deportiva que conecta personas a través del deporte. Crea tus propios eventos o únete a uno de ellos.",
+    url: "https://pleimeit.com",
+    siteName: "Pleimeit",
+    images: [
+      {
+        src: "/images/home/Logo-Pleimeit.png",
+        width: 1200,
+        height: 630,
+        alt: "Logo de Pleimeit, app deportiva",
+      },
+    ],
+    locale: "es_ES",
+    type: "website",
+  },
+};
+
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: {
@@ -29,6 +52,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PXRK6HZW');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`
           ${raleway.variable}
@@ -36,6 +75,16 @@ export default function RootLayout({
           ${bebas.variable}
         `}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PXRK6HZW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        
         {children}
       </body>
     </html>
